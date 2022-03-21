e101f719d68dcab08ad36d46b249ed4c5f23bfb1
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-03-21 01:19:46 +0100

    initial commit

---
+++ src/test/java/tech/woodandsafety/unserialjavademo/UnserialJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.woodandsafety.unserialjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnserialJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
