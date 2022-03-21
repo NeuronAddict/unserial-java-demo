315cd3844fd363c058d40fd0573e9ae72177f1fb
neuronaddict <neuronaddict@users.noreply.github.com>
2022-03-21 01:19:46 +0100

    initial commit

---
+++ src/main/java/tech/woodandsafety/unserialjavademo/UnserialJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.woodandsafety.unserialjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnserialJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnserialJavaDemoApplication.class, args);
+    }
+
+}
