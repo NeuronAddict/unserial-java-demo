b4d9b1f0397ab3dc694ce4024effac7ea27f83d8
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-03 03:18:25 +0200

    log4j vuln

--- src/main/java/tech/woodandsafety/unserialjavademo/HomeController.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/HomeController.java
@@ -1,5 +1,6 @@
 package tech.woodandsafety.unserialjavademo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.util.Base64;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping
     public String home(HttpServletRequest request, Model model) throws IOException {
+
+        log.info(String.format("Got user agent : %s", request.getHeader("User-Agent")));
+
         TrackingInfo ti = new TrackingInfo(request.getRemoteAddr(), request.getHeader("User-Agent"), "/");
         try(
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
