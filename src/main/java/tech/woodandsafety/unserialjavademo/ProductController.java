254a8d4254afd67dec4fbc219864490184e64fb6
neuronaddict <manufrommars@elacourarie.org>
2022-04-03 16:42:55 +0200

    product page

--- src/main/java/tech/woodandsafety/unserialjavademo/ProductController.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
@@ -11,19 +12,26 @@ import java.io.IOException;
 public class ProductController {
 
     private final TrackingService trackingService;
+    private final ProductService productService;
 
-    public ProductController(TrackingService trackingService) {
+    public ProductController(TrackingService trackingService, ProductService productService) {
         this.trackingService = trackingService;
+        this.productService = productService;
     }
 
     @GetMapping("/product")
     public String index(@RequestParam(required = false) String trackingData) throws IOException, ClassNotFoundException {
-        return "product";
+        return "products";
     }
 
     @GetMapping("/product/{id}")
-    public String index(@RequestParam(required = false) String trackingData, @PathVariable("id") Integer productId) throws IOException, ClassNotFoundException {
+    public ModelAndView index(@RequestParam(required = false) String trackingData, @PathVariable("id") Integer productId) throws IOException, ClassNotFoundException {
         if(trackingData != null) trackingService.readTrackingInfo(trackingData, productId);
-        return "laptop";
+        ModelAndView modelAndView = new ModelAndView("product");
+        Product product = productService.get(productId);
+        modelAndView.addObject("product", product);
+        modelAndView.addObject("track", trackingData);
+        modelAndView.addObject("image_src", String.format("/images/%s.jpg", product.getImage()));
+        return modelAndView;
     }
 }
