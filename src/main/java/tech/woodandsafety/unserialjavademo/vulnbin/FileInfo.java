2bae399d43f594dc733f9556f2dda4f8f91b2c1b
neuronaddict <manufrommars@elacourarie.org>
2022-04-03 22:11:44 +0200

    FileInfo gadget

---
+++ src/main/java/tech/woodandsafety/unserialjavademo/vulnbin/FileInfo.java
@@ -0,0 +1,53 @@
+package tech.woodandsafety.unserialjavademo.vulnbin;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.*;
+
+@ToString
+@EqualsAndHashCode
+public class FileInfo implements Serializable {
+
+    private final String filename;
+    private String fileType;
+
+    public FileInfo(String filename) throws IOException {
+        this.filename = filename;
+        initFileInfo(filename);
+    }
+
+    private void initFileInfo(String filename) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("/bin/sh");
+        pb.command().add("-c");
+        pb.command().add("file " + filename);
+        Process process = pb.start();
+        try(InputStreamReader isr = new InputStreamReader(process.getInputStream());
+            BufferedReader br = new BufferedReader(isr)) {
+            StringBuilder sb = new StringBuilder();
+            String s;
+            while ((s = br.readLine()) != null) {
+                sb.append(s).append("\n");
+            }
+            this.fileType = sb.toString();
+        }
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    private void writeObject(final ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        out.writeObject(this.filename);
+    }
+
+    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+        String filename = (String) in.readObject();
+        initFileInfo(filename);
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+}
