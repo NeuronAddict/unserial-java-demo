236983591767321f167fbc8b78de87f6e8069ba4
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-03 03:21:09 +0200

    safe mode

--- src/main/java/tech/woodandsafety/unserialjavademo/TrackingController.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/TrackingController.java
@@ -1,5 +1,7 @@
 package tech.woodandsafety.unserialjavademo;
 
+import org.securitywhitepapers.deserialization.LookAheadObjectInputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,24 @@ import java.util.Base64;
 @RestController("/")
 public class TrackingController {
 
+    private final boolean safe;
+
+    public TrackingController(@Value("${tech.woodandsafety.unserial.safe}") boolean safe) {
+        this.safe = safe;
+    }
+
+    private ObjectInputStream getObjectInputStream(ByteArrayInputStream bis, boolean safe) throws IOException {
+        if(safe) return new LookAheadObjectInputStream(bis);
+        else return new ObjectInputStream(bis);
+    }
+
     @PostMapping("track")
     public void sendTrackingInfo(@RequestParam String data) throws IOException, ClassNotFoundException {
         byte[] decoded = Base64.getDecoder().decode(data);
 
         try(
             ByteArrayInputStream bis = new ByteArrayInputStream(decoded);
-            ObjectInputStream ois = new ObjectInputStream(bis)) {
+            ObjectInputStream ois = getObjectInputStream(bis, safe)) {
             Object o = ois.readObject();
             TrackingInfo ti = (TrackingInfo) o;
 
