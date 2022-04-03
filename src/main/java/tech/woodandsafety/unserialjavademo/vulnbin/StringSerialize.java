92943436d3924c80a3cb2a1c80fffcdc07c28dbe
neuronaddict <manufrommars@elacourarie.org>
2022-04-04 01:57:52 +0200

    Use encoder in exploits

--- src/main/java/tech/woodandsafety/unserialjavademo/vulnbin/StringSerialize.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/vulnbin/StringSerialize.java
@@ -1,23 +1,13 @@
 package tech.woodandsafety.unserialjavademo.vulnbin;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.Base64;
+import tech.woodandsafety.unserialjavademo.tools.DataEncoder;
+import tech.woodandsafety.unserialjavademo.tools.SerializeEncoder;
 
 public final class StringSerialize {
 
-    public void main(String[] args) throws IOException {
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
-
-            System.out.println("FileInfo: ");
-            Object o = new FileInfo("/etc/passwd ; gnome-calculator");
-            System.out.println(o);
-            oos.writeObject(o);
-
-            System.out.println(new String(Base64.getEncoder().encode(bos.toByteArray())));
-        }
+    public static void main(String[] args) throws DataEncoder.EncodeException {
+        SerializeEncoder<String> encoder = new SerializeEncoder<>();
+        System.out.println(encoder.encode("coucou"));
     }
 
 }
