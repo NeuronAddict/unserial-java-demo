342230286e781fbd12093bcb11da29ac175fadf3
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-04 01:08:13 +0200

    typo

--- src/main/java/tech/woodandsafety/unserialjavademo/service/ProductService.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/service/ProductService.java
@@ -3,6 +3,7 @@ package tech.woodandsafety.unserialjavademo.service;
 import tech.woodandsafety.unserialjavademo.bean.Product;
 
 public interface ProductService {
+
     Product get(int id);
 
     Product[] all();
