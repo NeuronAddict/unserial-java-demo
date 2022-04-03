cf2e1ced95331e13fffb8790c07d540187cb6084
neuronaddict <manufrommars@elacourarie.org>
2022-04-03 22:37:21 +0200

    safe unserial

--- src/main/java/tech/woodandsafety/unserialjavademo/bean/TrackingInfo.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/bean/TrackingInfo.java
@@ -1,10 +1,12 @@
 package tech.woodandsafety.unserialjavademo.bean;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.io.Serializable;
 
 @Getter
+@EqualsAndHashCode
 public class TrackingInfo implements Serializable {
 
     private final String ip;
