6a8c8b05a6f0e82277ef9431a0eeb990c3d515d3
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-03 18:46:15 +0200

    log on tracking service

--- src/main/java/tech/woodandsafety/unserialjavademo/HomeController.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/HomeController.java
@@ -1,6 +1,5 @@
 package tech.woodandsafety.unserialjavademo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-@Slf4j
 @Controller("/")
 public class HomeController {
 
@@ -20,7 +18,6 @@ public class HomeController {
 
     @GetMapping
     public String home(HttpServletRequest request, Model model) throws IOException {
-        log.info(String.format("Got user agent : %s", request.getHeader("User-Agent")));
         model.addAttribute("track", trackingService.getTrackingInfo(request.getRemoteAddr(), request.getHeader("User-Agent")));
         return "index";
     }
