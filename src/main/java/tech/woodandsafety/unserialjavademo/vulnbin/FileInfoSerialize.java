8c0312042f76475ab80d0476c932230221404595
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-06 01:32:09 +0200

    Better example for FileInfoSerialize

--- src/main/java/tech/woodandsafety/unserialjavademo/vulnbin/FileInfoSerialize.java
+++ src/main/java/tech/woodandsafety/unserialjavademo/vulnbin/FileInfoSerialize.java
@@ -7,11 +7,16 @@ import java.io.IOException;
 
 public class FileInfoSerialize {
 
-    public static void main(String[] args) throws IOException, DataEncoder.EncodeException {
+    public static void main(String[] args) throws IOException, DataEncoder.EncodeException, DataEncoder.DecodeException {
         FileInfo fi = new FileInfo("/etc/passwd");
         SerializeEncoder<FileInfo> encoder = new SerializeEncoder<>();
 
-        System.out.println(encoder.encode(fi));
+        String encoded = encoder.encode(fi);
+        System.out.println(encoded);
+
+        FileInfo decoded = encoder.decode(encoded);
+
+        System.out.println(decoded);
 
     }
 
